9de0684af4bbecb2accef91e638ef75d90bffc26
Erik Lax <erik@datahack.se>
2017-05-04 16:03:33 +0200

    use correct namespace

--- rate.py
+++ rate.py
@@ -4,7 +4,7 @@ import sys, socket, struct, hashlib, hmac
 
 key = 'hash-key'
 
-m = struct.pack('!bbIII64s512s', 1, 4, 0, 0, 3600, '', sys.argv[1]);
+m = struct.pack('!bbIII64s512s', 1, 4, 0, 0, 3600, 'delivery-failures', sys.argv[1]);
 m += hmac.new(key.decode('hex'), m, hashlib.sha1).digest()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
